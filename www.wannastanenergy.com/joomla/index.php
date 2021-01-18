b509935fc1577431baafc060054c03db505d7393
R3alPlagu3 <r3alplagu3@W2216591.local>
2021-01-18 11:07:56 +0800

    Update

---
+++ www.wannastanenergy.com/joomla/index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb">
+<head>
+
+    
+	    
+       
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>  
+    <base href="http://www.wannastanenergy.com/joomla/index.php"/>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<meta name="keywords" content="joomla, Joomla"/>
+	<meta name="author" content="admin"/>
+	<meta name="description" content="Joomla! - the dynamic portal engine and content management system"/>
+	<meta name="generator" content="Joomla! - Open Source Content Management"/>
+	<title>Wannastan Energy Ltd - Home</title>
+	<link href="/joomla/templates/as002037free/css/A.bootstrap.css+tmpl.default.css+media.1200.css+bootstrap.responsive.css+media.980.css+media.768.css+media.480.css+style.default.css,Mcc.tJajqVf3Ai.css.pagespeed.cf.7Y3Wpsqo3F.css" rel="stylesheet" type="text/css"/>
+	
+	
+	
+	
+	
+	
+	
+	<script src="/joomla/media/jui/js/jquery.min.js,qc8bc6bb4b93c30e53cfe8fc11a18597b.pagespeed.jm.brnU6VEDeW.js" type="text/javascript"></script>
+	<script type="text/javascript">//<![CDATA[
+jQuery.noConflict();
+//]]></script>
+	<script src="/joomla/media/jui/js/jquery-migrate.min.js,qc8bc6bb4b93c30e53cfe8fc11a18597b.pagespeed.jm.C2obERNcWh.js" type="text/javascript"></script>
+	<script src="/joomla/media,_system,_js,_caption.js,qc8bc6bb4b93c30e53cfe8fc11a18597b+media,_jui,_js,_bootstrap.js+templates,_as002037free,_js,_jquery.isotope.min.js+templates,_as002037free,_js,_touch.gallery.js.pagespeed.jc.hivSBwNe4G.js"></script><script>eval(mod_pagespeed_XRAVj$TQXn);</script>
+	<script>eval(mod_pagespeed_qNiK7HWhXs);</script>
+	<script>eval(mod_pagespeed_gCTL2U9mub);</script>
+	<script>eval(mod_pagespeed_1sErSdBNT5);</script>
+	<script src="/joomla/templates/as002037free/js/scripts.js.pagespeed.jm.fyDoqK7AQg.js" type="text/javascript"></script>
+	<script type="text/javascript">jQuery(window).on('load',function(){new JCaption('img.caption');});jQuery.noConflict()</script>
+
+
+	
+<link href='https://fonts.googleapis.com/css?family=Open+Sans&subset=latin,greek,cyrillic' rel='stylesheet' type='text/css'><link href='https://fonts.googleapis.com/css?family=Tangerine&subset=latin,cyrillic,greek' rel='stylesheet' type='text/css'><style type="text/css">body{font-family:Tahoma,Geneva,sans-serif,Arial;font-size:12px}a,#featured-row .mod-newsflash .item:hover h2{}a:hover,a.selected{}input,button,select,textarea{font-family:Tahoma,Geneva,sans-serif,Arial}h1,h2,h3,h4,h5,.call-us,.item_header .item_title,#member-registration legend{font-family:'Open Sans',Arial,serif!important}h1{}h2{}h3,.item_header .item_title{}h4{}h5{}.categories-module li a,.archive-module li a{}.categories-module li a:hover,.archive-module li a:hover{}a.btn,.btn_info,.btn-info,.btn-group button.btn,.btn-primary,.btn_primary,.contentpane .button{}a.btn:hover,.btn_info:hover,.btn-info:hover,.btn_info:active,.btn-info:active,.btn_info.active,.btn-info.active,.btn_info.disabled,.btn-info.disabled,.btn_info[disabled],.btn-info[disabled],.btn-primary:hover,.btn_primary:hover,.btn-primary:active,.btn_primary:active,.btn-primary.active,.btn_primary.active,.btn-primary.disabled,.btn_primary.disabled,.btn-primary[disabled],.btn_primary[disabled],.contentpane .button:hover{}#header-row .moduletable.call-now{}#header-row .moduletable.call-now div{font-family:'Open Sans',Arial,serif!important}#header-row .logo{float:left;line-height:60px;min-width:240px}#header-row .logo,#header-row .logo a,#header-row .logo a:hover{font-family:'Tangerine',Arial,serif!important;font-style:normal;font-weight:normal}#header-row .logo span.slogan{left:5px;top:0;font-family:Arial,Helvetica,sans-serif;font-size:12px;font-style:normal;font-weight:normal}#footer-row ul.nav li a{}#footer-row ul.nav li a:hover{}#copyright-menu li a,#copyright-menu li.current a,#copyright-menu li.active a{}#copyright-menu li a:hover{}</style>
+    <link rel="stylesheet" href="/joomla/templates/as002037free/css/A.ext.asmenu.css.pagespeed.cf.OBYPrioTGY.css" type="text/css"/>
+    <link rel="stylesheet" href="/joomla/templates/as002037free/css/A.ext.artslider.css+style.custom.css,Mcc.uUK8oyTqrF.css.pagespeed.cf.5pFaUIF9Wf.css" type="text/css"/>    
+    
+  
+</head>
+
+<body class="com_content view-article task- itemid-108 body-">
+	<div class="main-wrapper">
+    	<!-- HEADER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  -->    
+    	<div id="top-header">
+            <div class="top-wrapper">
+                <div class="container">
+                    
+                </div>
+            </div>
+        </div>
+        <div id="header-row" class="solid">
+            <div id="header-wrapper">
+                <div class="container">
+                    
+                    <div id="top-search">
+                        
+                    </div>
+                    <div class="row-fluid">
+                    
+                        <div id="logo" class="span4">
+                                                        <a href="/joomla">
+                                <script data-pagespeed-no-defer>//<![CDATA[
+(function(){for(var g="function"==typeof Object.defineProperties?Object.defineProperty:function(b,c,a){if(a.get||a.set)throw new TypeError("ES3 does not support getters and setters.");b!=Array.prototype&&b!=Object.prototype&&(b[c]=a.value)},h="undefined"!=typeof window&&window===this?this:"undefined"!=typeof global&&null!=global?global:this,k=["String","prototype","repeat"],l=0;l<k.length-1;l++){var m=k[l];m in h||(h[m]={});h=h[m]}var n=k[k.length-1],p=h[n],q=p?p:function(b){var c;if(null==this)throw new TypeError("The 'this' value for String.prototype.repeat must not be null or undefined");c=this+"";if(0>b||1342177279<b)throw new RangeError("Invalid count value");b|=0;for(var a="";b;)if(b&1&&(a+=c),b>>>=1)c+=c;return a};q!=p&&null!=q&&g(h,n,{configurable:!0,writable:!0,value:q});var t=this;function u(b,c){var a=b.split("."),d=t;a[0]in d||!d.execScript||d.execScript("var "+a[0]);for(var e;a.length&&(e=a.shift());)a.length||void 0===c?d[e]?d=d[e]:d=d[e]={}:d[e]=c};function v(b){var c=b.length;if(0<c){for(var a=Array(c),d=0;d<c;d++)a[d]=b[d];return a}return[]};function w(b){var c=window;if(c.addEventListener)c.addEventListener("load",b,!1);else if(c.attachEvent)c.attachEvent("onload",b);else{var a=c.onload;c.onload=function(){b.call(this);a&&a.call(this)}}};var x;function y(b,c,a,d,e){this.h=b;this.j=c;this.l=a;this.f=e;this.g={height:window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,width:window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth};this.i=d;this.b={};this.a=[];this.c={}}function z(b,c){var a,d,e=c.getAttribute("data-pagespeed-url-hash");if(a=e&&!(e in b.c))if(0>=c.offsetWidth&&0>=c.offsetHeight)a=!1;else{d=c.getBoundingClientRect();var f=document.body;a=d.top+("pageYOffset"in window?window.pageYOffset:(document.documentElement||f.parentNode||f).scrollTop);d=d.left+("pageXOffset"in window?window.pageXOffset:(document.documentElement||f.parentNode||f).scrollLeft);f=a.toString()+","+d;b.b.hasOwnProperty(f)?a=!1:(b.b[f]=!0,a=a<=b.g.height&&d<=b.g.width)}a&&(b.a.push(e),b.c[e]=!0)}y.prototype.checkImageForCriticality=function(b){b.getBoundingClientRect&&z(this,b)};u("pagespeed.CriticalImages.checkImageForCriticality",function(b){x.checkImageForCriticality(b)});u("pagespeed.CriticalImages.checkCriticalImages",function(){A(x)});function A(b){b.b={};for(var c=["IMG","INPUT"],a=[],d=0;d<c.length;++d)a=a.concat(v(document.getElementsByTagName(c[d])));if(a.length&&a[0].getBoundingClientRect){for(d=0;c=a[d];++d)z(b,c);a="oh="+b.l;b.f&&(a+="&n="+b.f);if(c=!!b.a.length)for(a+="&ci="+encodeURIComponent(b.a[0]),d=1;d<b.a.length;++d){var e=","+encodeURIComponent(b.a[d]);131072>=a.length+e.length&&(a+=e)}b.i&&(e="&rd="+encodeURIComponent(JSON.stringify(B())),131072>=a.length+e.length&&(a+=e),c=!0);C=a;if(c){d=b.h;b=b.j;var f;if(window.XMLHttpRequest)f=new XMLHttpRequest;else if(window.ActiveXObject)try{f=new ActiveXObject("Msxml2.XMLHTTP")}catch(r){try{f=new ActiveXObject("Microsoft.XMLHTTP")}catch(D){}}f&&(f.open("POST",d+(-1==d.indexOf("?")?"?":"&")+"url="+encodeURIComponent(b)),f.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),f.send(a))}}}function B(){var b={},c;c=document.getElementsByTagName("IMG");if(!c.length)return{};var a=c[0];if(!("naturalWidth"in a&&"naturalHeight"in a))return{};for(var d=0;a=c[d];++d){var e=a.getAttribute("data-pagespeed-url-hash");e&&(!(e in b)&&0<a.width&&0<a.height&&0<a.naturalWidth&&0<a.naturalHeight||e in b&&a.width>=b[e].o&&a.height>=b[e].m)&&(b[e]={rw:a.width,rh:a.height,ow:a.naturalWidth,oh:a.naturalHeight})}return b}var C="";u("pagespeed.CriticalImages.getBeaconData",function(){return C});u("pagespeed.CriticalImages.Run",function(b,c,a,d,e,f){var r=new y(b,c,a,e,f);x=r;d&&w(function(){window.setTimeout(function(){A(r)},0)})});})();pagespeed.CriticalImages.Run('/mod_pagespeed_beacon','http://www.wannastanenergy.com/joomla/index.php','8Xxa2XQLv9',true,false,'gMnuaXDkh4c');
+//]]></script><img src="http://www.wannastanenergy.com/joomla/images/xWEL.jpg.pagespeed.ic.9HjALPrSCq.webp" alt="Wannastan Energy Ltd" data-pagespeed-url-hash="718871753" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"/>
+                            </a>
+                                                        
+                        </div>
+                                     
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+   
+        <div id="breadcrumbs-row">
+	       	</div>
+        
+                <div id="slider-row" class="top-title">
+            <div class="top-wrapper"> 
+                <div class="container"> 
+                    <div class="row-fluid">
+                        <h1 id="title">Wannastan Energy Ltd - Home</h1>
+                    </div>
+                </div> 
+            </div>   
+        </div>
+             
+
+    	<!-- END OF HEADER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ --> 
+    	
+
+       <div id="content-wrapper">     
+           <div class="main">
+                
+                                
+               
+            
+                                       
+            
+                <!-- CONTENT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+                <div id="content-row">
+                    <div class="container">
+                        <div class="content-inner row">
+                        
+                            <!-- COLUMN LEFT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+                            
+                                                        <!-- END OF COlUMN LEFT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+                                    
+                            <!-- COLUMN MAIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->                
+                            <div id="component" class="span12">
+                        
+                                                                                                <div class="row">
+                                    <div id="system-message-container">
+	</div>
+
+                                    <div class="page-item page-item__">
+
+					
+
+	
+		
+		
+			
+	
+
+		
+	<div class="item_fulltext"><div class="bg"><img src="/joomla/images/xOwned_by_Viper.png.pagespeed.ic.6P8-GEX1VU.webp" alt="" data-pagespeed-url-hash="1788011035" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"/> </div>
+<p> </p> </div>
+
+	
+						 </div>
+
+                                </div>
+                                                                
+                            </div>
+                            <!-- END OF COLUMN MAIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+            
+                            <!-- COLUMN RIGHT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->                    
+                                                        <!-- END OF COLUMN RIGHT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+                        </div>
+                    </div>
+                </div>
+                
+                   
+                        
+                                
+                                
+              
+             </div> <!-- end main -->
+         </div> <!-- end content-wrapper -->
+          <!-- FOOTER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+          
+          <div id="footer">
+                                
+              <div id="copyright-row">
+              <div class="container">   
+                    <div id="trademark">
+                        Copyright &copy; 2021 Wannastan Energy Ltd                    </div>
+                    <div id="copyright-menu" class="row">
+                        <!-- DO NOT REMOVE OR CHANGE THE CONTENT BELOW, THIS THEME MAY NOT WORK PROPERLY -->
+                        
+                            <div id="ascopy">
+                            <a href="http://www.astemplates.com/" target="_blank">
+                                &nbsp;&nbsp;&nbsp;&nbsp;DESIGNED BY:&nbsp;&nbsp;AS DESIGNING
+                            </a>
+                            </div>
+                        
+                        <!-- DO NOT REMOVE OR CHANGE THE CONTENT ABOVE, THIS THEME MAY NOT WORK PROPERLY -->
+                    </div> 
+              </div>   
+              </div>
+          </div>
+        
+        <!-- END OF FOOTER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->    
+        
+    <!-- END OF main container ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    
+        <div id="back-top">
+    	<a href="#"><span></span></a>
+    </div>
+        
+        
+            
+    	</div>
+</body>
+</html>
